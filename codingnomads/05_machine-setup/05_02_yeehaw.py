701e1ca2bec7b3beea826c2c871202d8fd59e082
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-07-21 20:28:59 -0400

    Delete codingnomads/05_machine-setup directory

--- codingnomads/05_machine-setup/05_02_yeehaw.py
+++
@@ -1,9 +0,0 @@
-# In this script, write code that displays the follow message:
-#
-# 	I'm a programmer now.
-# 	Yeehaw!
-# 	Coding here I come!
-
-print('I\'m a programmer now')
-print('Yeehaw!')
-print('Coding here I come!')
--- codingnomads/05_machine-setup/05_04_seconds_year.py
+++
@@ -1,4 +0,0 @@
-# Use the interpreter to calculate how many seconds are in a year.
-# Then write the code in this script file below this line.
-seconds = 60 * 60 * 24 * 365
-print(seconds)
