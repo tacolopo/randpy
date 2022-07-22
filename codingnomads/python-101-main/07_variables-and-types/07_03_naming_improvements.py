57a1e7506869a8d0a9427378882b0e81f0d14e8b
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-07-21 20:32:28 -0400

    Add files via upload

---
+++ codingnomads/python-101-main/07_variables-and-types/07_03_naming_improvements.py
@@ -0,0 +1,9 @@
+# The code below works as expected and there are no errors.
+# However, the variable names aren't great and don't follow the guidelines.
+# Improve the variable names so they follow the PEP-8 guidelines.
+
+name = "Ada"
+last_name = "Lovelace"
+full_name = name + " " + last_name
+
+print(full_name)
---
+++ codingnomads/python-101-main/07_variables-and-types/07_04_multiple_improve.py
@@ -0,0 +1,7 @@
+# Improve the variable assignment shown below.
+# Why should you change it?
+
+color = 'red'
+year = 1900
+
+#variables are unrelated and therefore should be on separate lines
