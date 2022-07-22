2bfc7f33ae4d0cb6cc71ec3c7069de6a052fc0a7
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-07-21 20:31:00 -0400

    Add files via upload

---
+++ codingnomads/python-101-main/05_machine-setup/05_02_yeehaw.py
@@ -0,0 +1,9 @@
+# In this script, write code that displays the follow message:
+#
+# 	I'm a programmer now.
+# 	Yeehaw!
+# 	Coding here I come!
+
+print('I\'m a programmer now')
+print('Yeehaw!')
+print('Coding here I come!')
---
+++ codingnomads/python-101-main/05_machine-setup/05_04_seconds_year.py
@@ -0,0 +1,4 @@
+# Use the interpreter to calculate how many seconds are in a year.
+# Then write the code in this script file below this line.
+seconds = 60 * 60 * 24 * 365
+print(seconds)
