d07c19e5da52217ef06ca429b9ad4326423069b2
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-07-21 20:29:37 -0400

    Add files via upload

---
+++ codingnomads/python-101-main/04_build_something/guess.py
@@ -0,0 +1,22 @@
+# All the text in here are Python code comments.
+# A Python code comment starts with a hash symbol (#).
+# Python will ignore this when running the file.
+# You'll see instructions for your labs written in code comments.
+# --------------------------------
+# Here's your first task:
+# Re-create the guess-my-number game from the course.
+# Type the whole code out instead of copy-pasting.
+# Typing out code, even if you just copy it, trains your coding skills!
+# Write your code below:
+import random
+num = random.randint(0,10)
+guess = None
+
+while guess != num:
+    guess = input('Enter between 1 and 10:')
+    guess = int(guess)
+    if guess == num:
+        print('correct')
+    else:
+        print('Incorrect')
+        continue
